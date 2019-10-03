9221992af355dfde8cfd08d106b794bfb52beaee
nitish chauhan <ncds42@gmail.com>
2019-10-03 01:15:34 -0500

    need module in routes folder so keystone doesnt break

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// const path = require("path");
+
+// // Export our app routes
+// exports = module.exports = function(app) {
+//   // app.all('/api*', keystone.middleware.cors);
+//   // app.options('/api*', function(req, res) { res.send(200); });
+
+//   // Get request pointing to our api (test using postman)
+//   app.get("/", function(req, res) {
+//     // Render some simple boilerplate html
+//     res.sendFile(path.join(__dirname, "../../public/index.html"));
+//   });
+// };
