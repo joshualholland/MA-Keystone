f4b0802764f670633b504187996321d9990638fd
Josh <joshualholland@gmail.com>
2019-10-05 21:55:47 -0500

    removed unnecessary index.html

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "74ff4f0f0c5c0fc965f5";
+/******/ 	var hotCurrentHash = "75fc23d0abee5f2ecaa2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
