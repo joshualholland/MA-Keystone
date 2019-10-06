77f3068b26723b69904751a3808c9cb001459f31
nitish chauhan <ncds42@gmail.com>
2019-10-06 08:33:48 -0500

    added scss,css,file loaders to config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  /* config options here */
-});
+const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
+const withImages = require('next-images')
+
+// Why I set up like this
+// https://dev.to/harveyjones282/the-simplest-way-to-configure-next-js-with-sass-3en
+// https://github.com/zeit/next.js/issues/3852
+module.exports = withCSS(
+  withSass(
+    withImages({
+      webpack(config, options) {
+     
+        return config;
+      }
+    })
+  )
+);
