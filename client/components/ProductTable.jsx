596b5c1924ba79736f94b47e38309669b813ae81
nitish chauhan <ncds42@gmail.com>
2019-10-16 00:59:32 -0500

    finished services page. still needs responsiveness

--- client/components/ProductTable.jsx
+++ client/components/ProductTable.jsx
@@ -1,27 +1,21 @@
 import React from "react";
 
 export default function ProductTable(props) {
-  console.log(props)
-  console.log(props.products[0].name)
-
-  function productFactory(props) {
-    const products = props.products;
-    const items = [];
-    for (let i; i < products.length; i++) {
-      console.log(products[i].name);
-      items.push(
-        <tr className="productRow">
-          <td className="product">{products[i].name}</td>
-          <td className="price">{products[i].price}</td>
-        </tr>
-      );
-    }
-    return items;
-  }
+  console.log(props);
+  const products = props.products;
+  console.log(products);
+  console.log(products.length);
 
   return (
     <table className="productTable">
-      <tbody>{productFactory(props)}</tbody>
+      {products.map((product) => {
+        return (
+          <tr className='productRow'>
+            <td className='productName'>{product.name}</td>
+            <td className='productPrice'>{product.price}</td>
+          </tr>
+        );
+      })}
     </table>
   );
 }
--- pages/services.jsx
+++ pages/services.jsx
@@ -2,26 +2,24 @@ import React from "react";
 import Navbar from "../client/components/Navbar";
 import Banner from "../client/components/Banner";
 import ProductTable from "../client/components/ProductTable";
+import Footer from "../client/components/Footer";
 import "../client/scss/app.scss";
 
 export default function Services() {
-  
-  const Product = function(name,price){
+  const Product = function(name, price) {
     this.name = name;
     this.price = price;
-  }
+  };
 
   const products = [];
 
-  for(let i = 0;i<5;i++){
-    let prod = new Product('Hair Cut','$20');
+  for (let i = 0; i < 5; i++) {
+    let prod = new Product("Hair Cut", "$20");
     products.push(prod);
     // products.push({'Hair Cut':'$20'})
   }
-  console.log(products)
+  // console.log(products)
 
-  
-  
   return (
     <>
       <Navbar />
@@ -30,16 +28,16 @@ export default function Services() {
         sectionName={"services"}
         content={"SERVICES"}
       />
-      <section className='text-center mt-5 pt-5'>
-        <h3 className='px-3'>We strive to make you feel and look the best!!</h3>
-        <p className='px-3 mt-3'>
+      <section className="text-center mt-5 pt-5">
+        <h3 className="px-3">We strive to make you feel and look the best!!</h3>
+        <p className="px-3 mt-3 container">
           We offer a free consultation for any service we perform. This is a
           time where you can come in and discuss with your stylist what you have
           dreamed of for your hair and can be scheduled directly before your
           appointment time or in advance. Our treat!
         </p>
-        <h5 className='mt-4'>Cancellation Policy</h5>
-        <p className='px-3 mt-3'>
+        <h5 className="mt-4">Cancellation Policy</h5>
+        <p className="px-3 mt-3 container">
           We may require a credit card to reserve appointments. In order to
           avoid a service charge, and ensure your appointments for other
           clients, please reschedule or cancel appointments before the 24 hour
@@ -49,12 +47,24 @@ export default function Services() {
           on file will be charged 50% of booked services.
         </p>
       </section>
-      <section className='text-center productHeader'>
-        <h5>SERVICES</h5>
-      </section>
+      <h5 className="text-center productHeader">SERVICES</h5>
       <section className=''>
-        <ProductTable products={products}/>
+        <ProductTable products={products} />
       </section>
+      <h5 className="text-center productHeader">BRIDALS</h5>
+      <section className="mb-5">
+        <ProductTable products={products} />
+      </section>
+      <section className="productsImageContainer">
+        <div className='w-100 h-100'
+        style={{'background-color':'rgba(0,0,0,0.6)'}}>
+          <h5 className="px-3 pt-5 productsImageHeader">
+            ENJOY AN UNFORGETTABLE SALON EXPERIENCE
+          </h5>
+          <button className='btn bookButton mt-4'>BOOK AN APPOINTMENT</button>
+          </div>
+      </section>
+      <Footer/>
     </>
   );
 }
