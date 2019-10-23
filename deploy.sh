84e899dd249cc78af1b752fa125bbd6982453d31
nitish chauhan <ncds42@gmail.com>
2019-10-22 21:08:46 -0500

    setting up prod build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 echo 'Deploying to production repo'
 rm -rf ~/vs-workspace/morganAshleyDeploy/*
+rm -rf ~/vs-workspace/morganAshleyDeploy/.*
 cp -avr client ../morganAshleyDeploy/client
 cp -avr customModules ../morganAshleyDeploy/customModules
 cp -avr models ../morganAshleyDeploy/models
@@ -8,7 +9,8 @@ cp -avr public ../morganAshleyDeploy/public
 cp -avr updates ../morganAshleyDeploy/updates
 cp -avr routes ../morganAshleyDeploy/routes
 cp -av keys.json ../morganAshleyDeploy/keys.json
+cp -av .env ../morganAshleyDeploy/.env
 cp -av keystone.js ../morganAshleyDeploy/keystone.js
 cp -av next.config.js ../morganAshleyDeploy/next.config.js
 cp -av package.json ../morganAshleyDeploy/package.json
-printf "node_modules\npackage-lock.json\n.next\npublic/stylists/\npublic/team/\n" >> ../morganAshleyDeploy/.gitignore
+printf "node_modules\npackage-lock.json\npublic/stylists/\npublic/team/\n" >> ../morganAshleyDeploy/.gitignore
