f248cc60e02fe55c516ca060805b407602672d8f
nitish chauhan <ncds42@gmail.com>
2019-10-22 20:31:14 -0500

    added deploy script and changed npm start to default running script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+echo 'Deploying to production repo'
+rm -rf ~/vs-workspace/morganAshleyDeploy/*
+cp -avr client ../morganAshleyDeploy/client
+cp -avr customModules ../morganAshleyDeploy/customModules
+cp -avr models ../morganAshleyDeploy/models
+cp -avr pages ../morganAshleyDeploy/pages
+cp -avr public ../morganAshleyDeploy/public
+cp -avr updates ../morganAshleyDeploy/updates
+cp -avr routes ../morganAshleyDeploy/routes
+cp -av keys.json ../morganAshleyDeploy/keys.json
+cp -av keystone.js ../morganAshleyDeploy/keystone.js
+cp -av next.config.js ../morganAshleyDeploy/next.config.js
+cp -av package.json ../morganAshleyDeploy/package.json
+printf "node_modules\npackage-lock.json\n.next\npublic/stylists/\npublic/team/\n" >> ../morganAshleyDeploy/.gitignore
