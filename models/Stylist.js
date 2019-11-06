34f07f65a1881ef6eda617275e865c8f329ea93e
Josh <joshualholland@gmail.com>
2019-11-06 13:27:11 -0600

    setting up for easier deploy

--- models/Stylist.js
+++ models/Stylist.js
@@ -4,38 +4,6 @@ var Types = keystone.Field.Types;
 // Creates list for stylists
 var Stylist = new keystone.List("Stylist");
 
-// Storage for small images
-// var smallImgStorage = new keystone.Storage({
-//     adapter: keystone.Storage.Adapters.FS,
-//     fs: {
-//         // required; path where the files should be stored
-//         path: keystone.expandPath('./public/stylists'),
-//         generateFilename: function (file, index) {
-//             return file.originalname;
-//         },
-//         whenExists: 'overwrite',
-//         // path where files will be served
-//         publicPath: './public/stylists',
-//     },
-// });
-
-// Storage for larger images
-// var largeImgStorage = new keystone.Storage({
-//     adapter: keystone.Storage.Adapters.FS,
-//     fs: {
-//         // required; path where the files should be stored
-//         path: keystone.expandPath('./public/stylists'),
-//         generateFilename: function (file, index) {
-//             return file.originalname;
-//         },
-//         whenExists: 'overwrite',
-//         // path where files will be served
-//         publicPath: './public/stylists',
-//     },
-// });
-
-// Creates fields for stylists
-// May be necessary to update these for pre and post popup about
 Stylist.add({
   name: { type: Types.Name, required: true },
   start_date: { type: Types.Date },
